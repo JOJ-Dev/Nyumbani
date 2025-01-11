00e1c8f811cc0c509d86111b12e3c9727170dc49
George <georgem.gichuru@gmail.com>
2025-01-11 18:14:44 +0300

    'new'

--- Nyumbani/Base/urls.py
+++ Nyumbani/Base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from . import views
+from Base import views
 
 urlpatterns = [
     path('', views.index, name='index'),
--- Nyumbani/Nyumbani/settings.py
+++ Nyumbani/Nyumbani/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'Payments',
     #Modules
     'widget_tweaks',
+    'bootstrap5',
     # 'django_allauth',
     # 'django_allauth.account',
     # 'django_allauth.account.social',
@@ -64,9 +65,7 @@ ROOT_URLCONF = 'Nyumbani.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR, 'templates',
-            ],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [
-    BASE_DIR, 'static',
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
